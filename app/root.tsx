2e734e23105bd8386f1038fd62d039d53de24bf3
Rádler Ákos <akos.radler@gmail.com>
2024-09-21 08:46:19 +0200

    Init Remix with MUI

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,10 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
